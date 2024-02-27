4b593a1b8578f86b78dd833da5cd07bc2d793d81
DoGGoverHoEs <oxgodxo2002@gmail.com>
2024-02-28 02:28:01 +0700

    page manage user

--- src/pages/manageUsers/components/tableRowDataUsers/index.jsx
+++ src/pages/manageUsers/components/tableRowDataUsers/index.jsx
@@ -3,79 +3,154 @@ import {
   Badge,
   Button,
   Flex,
+  Tag,
+  TagLabel,
   Td,
   Text,
   Tr,
   useColorModeValue,
+  useToast,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useContext, useState } from "react";
+import ModalUser from "../modalUser";
+import { SystemUserControllerApi } from "../../../../api/generated/generate-api";
+import ApiClientSingleton from "../../../../api/apiClientImpl";
+import { UserContext } from "../..";
+import { TOAST } from "../../../../constant";
 
-function TableRowDataUsers(props) {
-  const { logo, name, email, subdomain, domain, status, date } = props;
+const userApi = new SystemUserControllerApi(ApiClientSingleton.getInstance());
+function TableRowDataUsers({ userInfo, roles }) {
+  const { id, name, email, phone, photo, active, roleKind } = userInfo;
   const textColor = useColorModeValue("gray.700", "white");
   const bgStatus = useColorModeValue("gray.400", "#1a202c");
   const colorStatus = useColorModeValue("white", "gray.400");
+  const [isOpenModalUser, setIsOpenModalUser] = useState(false);
+  const [userSelect, setUserSelect] = useState();
+  const { reload, setReload } = useContext(UserContext);
+  const toast = useToast();
 
   return (
-    <Tr>
-      <Td minWidth={{ sm: "160px" }} pl="0px" py={".6rem"}>
-        <Flex align="center" py=".4rem" minWidth="100%" flexWrap="nowrap">
-          <Avatar src={logo} w="50px" borderRadius="12px" me="18px" />
-          <Flex direction="column">
-            <Text
-              fontSize="md"
-              color={textColor}
-              fontWeight="bold"
-              minWidth="100%"
-            >
-              {name}
+    <>
+      <Tr>
+        <Td minWidth={{ sm: "160px" }} pl="0px" py={".6rem"}>
+          <Flex
+            justifyContent={"start"}
+            align="center"
+            py=".4rem"
+            minWidth="100%"
+            flexWrap="nowrap"
+          >
+            <Avatar src={photo} w="50px" borderRadius="12px" me="18px" />
+            <Flex direction="column">
+              <Text
+                fontSize="md"
+                color={textColor}
+                fontWeight="bold"
+                minWidth="100%"
+              >
+                {name}
+              </Text>
+            </Flex>
+          </Flex>
+        </Td>
+
+        <Td>
+          <Flex alignItems={"center"} direction="column">
+            <Text fontSize="md" color={textColor} fontWeight="bold">
+              {phone}
             </Text>
-            <Text fontSize="sm" color="gray.400" fontWeight="normal">
+          </Flex>
+        </Td>
+        <Td>
+          <Flex alignItems={"center"} direction="column">
+            <Text fontSize="md" color={textColor} fontWeight="bold">
               {email}
             </Text>
           </Flex>
-        </Flex>
-      </Td>
-
-      <Td>
-        <Flex direction="column">
-          <Text fontSize="md" color={textColor} fontWeight="bold">
-            {domain}
-          </Text>
-          <Text fontSize="sm" color="gray.400" fontWeight="normal">
-            {subdomain}
-          </Text>
-        </Flex>
-      </Td>
-      <Td>
-        <Badge
-          bg={status === "Online" ? "green.400" : bgStatus}
-          color={status === "Online" ? "white" : colorStatus}
-          fontSize="16px"
-          p="3px 10px"
-          borderRadius="8px"
-        >
-          {status}
-        </Badge>
-      </Td>
-      <Td>
-        <Text fontSize="md" color={textColor} fontWeight="bold" pb=".5rem">
-          {date}
-        </Text>
-      </Td>
-      <Td>
-        <Button p="0px" bg="transparent" variant="no-hover">
-          <Text
-            fontSize="md"
-            color="gray.400"
-            fontWeight="bold"
-            cursor="pointer"
-          >
-            Edit
-          </Text>
-        </Button>
-      </Td>
-    </Tr>
+        </Td>
+        <Td>
+          <Flex alignItems={"center"} direction="column">
+            <Text fontSize="md" color={textColor} fontWeight="bold" pb=".5rem">
+              {roleKind}
+            </Text>
+          </Flex>
+        </Td>
+        <Td>
+          <Flex alignItems={"center"} direction="column">
+            <Badge
+              bg={active ? "green.400" : bgStatus}
+              color={active ? "white" : colorStatus}
+              fontSize="16px"
+              p="3px 10px"
+              borderRadius="8px"
+            >
+              {active ? "Hoạt động" : "Đã khoá"}
+            </Badge>
+          </Flex>
+        </Td>
+        <Td>
+          <Flex justifyContent={"center"} gap={"8px"}>
+            <Button p="0px" bg="transparent" variant="no-hover">
+              <Tag size={"lg"} variant="outline" colorScheme="gray">
+                <TagLabel>Chi tiết</TagLabel>
+              </Tag>
+            </Button>
+            <Button
+              p="0px"
+              bg="transparent"
+              variant="no-hover"
+              onClick={() => {
+                userApi.systemUserControllerGetDetailsById(
+                  id,
+                  (err, data, response) => {
+                    const res = data?.data;
+                    setUserSelect(res);
+                  }
+                );
+                setIsOpenModalUser(true);
+              }}
+            >
+              <Tag size={"lg"} variant="outline" colorScheme="blue">
+                <TagLabel>Cập nhật</TagLabel>
+              </Tag>
+            </Button>
+            <Button
+              p="0px"
+              bg="transparent"
+              variant="no-hover"
+              onClick={() => {}}
+            >
+              <Tag size={"lg"} variant="outline" colorScheme="red">
+                <TagLabel>Xoá</TagLabel>
+              </Tag>
+            </Button>
+          </Flex>
+        </Td>
+      </Tr>
+      <ModalUser
+        isCreate={false}
+        title={"Cập nhật thông tin"}
+        isOpen={isOpenModalUser}
+        setOpen={setIsOpenModalUser}
+        user={userSelect}
+        setUser={setUserSelect}
+        roles={roles}
+        onFinish={() => {
+          userApi.systemUserControllerUpdateModel(
+            userSelect,
+            (err, data, res) => {
+              if (data) {
+                setIsOpenModalUser(false);
+                setReload(!reload);
+                TOAST.success(toast, "User", "Cập nhật người dùng thành công");
+              } else {
+                TOAST.error(toast, "User", "Cập nhật người dùng thất bại");
+              }
+            }
+          );
+        }}
+      />
+    </>
   );
 }
 
